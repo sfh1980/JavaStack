0c7a09343f56e9e5206848224ea51e2e04e53713
Sean <sfh1980@yahoo.com>
2022-09-24 17:15:44 -0400

    first commit

---
+++ Java_Demo/goodbyepy.py
@@ -0,0 +1,5 @@
+number = 23
+a_string = "bye bye python!"
+is_true = True
+
+print(number)
---
+++ Java_Demo/hellojava.java
@@ -0,0 +1,6 @@
+public class hellojava{
+    public static void main(String[] args){
+        int number = 23;
+        System.out.println(number);
+    }
+}
